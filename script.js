6316166eda9f09742f2ffa0bc953b4f5c1b94464
siebecluyts <debendevanzelem1@gmail.com>
2024-12-11 17:39:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelector('.cool-button').addEventListener('click', function (e) {
+    const button = e.currentTarget;
+  
+    // Add the sliding animation by modifying its position
+    button.style.transition = 'transform 0.5s ease';
+    button.style.transform = 'translateX(300px)';
+  
+    // Redirect after the animation ends
+    setTimeout(() => {
+      window.location.href = '/pressed.html';
+    }, 500); // Delay matches the animation duration
+  });
+  
